26e81c6eee8959970719ce6ffaf7f6059a176c0f
matiboy <mathieu.chauvinc@gmail.com>
2018-03-13 15:37:30 +0800

    Delay to show switchMap cancelling

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -3,6 +3,7 @@ import {Http} from '@angular/http'
 import {Observable} from 'rxjs' 
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/do'
+import 'rxjs/add/operator/delay'
 
 
 const BASE_URL = 'https://reqres.in/api/'
@@ -18,8 +19,12 @@ export class UsersService {
   }
 
   getUsers(page=1): Observable<User[]> {
-    return this.http.get(`${BASE_URL}users?page=${page}`)
-      .map(response => response.json())
-      .map(body => body.data)
+    // Delay by 3 seconds without emitting anything. Should allow to see the cancellation by switchMap
+    return Observable.empty().delay(Math.random() * 3000)
+      .concat(
+        this.http.get(`${BASE_URL}users?page=${page}`)
+        .map(response => response.json())
+        .map(body => body.data)
+      )
   }
 }
